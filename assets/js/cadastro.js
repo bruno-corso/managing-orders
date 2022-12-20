8442d144a9d3876f563fcefc29272064491bb4e9
btcorso <uniebcorso@fei.edu.br>
2022-12-20 08:17:02 -0300

    index criado + prévia js do cadastar

---
+++ assets/js/cadastro.js
@@ -0,0 +1,40 @@
+// console.log("hello world!");
+
+// fetch('../menu-cardapio.json').then(function (response) {
+//     return response.json();
+// }).then(function (obj) {
+//     itensCardapio = obj.itens;
+//     console.log(itensCardapio);
+
+//     function buscaItem(item) {
+//         itensCardapio.forEach(element => {
+//             if(element.nome == item){
+
+
+//             }
+//         });
+//     }
+
+//     buscaItem("Pastel Especial");
+// });
+
+// const inputs = document.querySelectorAll(".cadastro__input");
+// const botaoCadastro = document.querySelector("button");
+
+// botaoCadastro.addEventListener("click", () => {
+//     if (checaVazia() == "") {
+//         location.replace("../index.html");
+//     } else {
+//         alert("Preencha todos os campos.")
+//     };
+// });
+
+// function checaVazia() {
+//     var msg = '';
+//     inputs.forEach((e) => {
+//         if (e.value == '') {
+//             msg = "Campo Vazio."
+//         }
+//     })
+//     return msg;
+// }
